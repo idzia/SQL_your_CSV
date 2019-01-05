13a97950ff5bdaa54eda99a1f6167cdbcaf57ea9
idzia <ania.idzi@gmail.com>
2019-01-05 03:03:15 +0100

    Add where condition management

--- src/main/java/com/codecool/dao/DataDaoStream.java
+++ src/main/java/com/codecool/dao/DataDaoStream.java
@@ -1,27 +1,20 @@
 package com.codecool.dao;
 
 import java.util.*;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 
 public class DataDaoStream implements DataDao {
 
-    //private List<List<String>> csvStringList;
     private Map<String, Integer> fieldsNameMap;
-    private Reader fileData;
-
+    private Reader reader;
 
     public static final Integer HEADERS_INDEX  = 0;
 
-
-//    public DataDaoStream(List<List<String>> csvStringList) {
-//
-//        this.csvStringList = csvStringList;
-//        this.fieldsNameMap = getFieldsNameMap();
-//    }
-
-    public DataDaoStream(Reader fileData, String fileName) {
-        this.fileData = fileData;
+    public DataDaoStream(Reader reader, String fileName) {
+        this.reader = reader;
         this.fieldsNameMap = getFieldsNameMap(fileName);
     }
 
@@ -36,27 +29,8 @@ public class DataDaoStream implements DataDao {
         return fieldsNameMap;
     }
 
-//    public Map<String, Integer> getFieldsNameMap() {
-//        List<String> fieldsNameList = getFieldsNameList();
-//        Map<String, Integer> fieldsNameMap = new HashMap<>();
-//
-//        for(String item : fieldsNameList) {
-//            fieldsNameMap.put(item.toLowerCase(), fieldsNameList.indexOf(item));
-//        }
-//
-//        return fieldsNameMap;
-//    }
-//
-//    private List<String> getFieldsNameList() {
-//        List<String> fieldsNameList = csvStringList.get(HEADERS_INDEX).stream()
-//                .map(item -> item.toLowerCase())
-//                .collect(Collectors.toList());
-//
-//        return fieldsNameList;
-//    }
-
     private List<String> getFieldsNameList(String fileName) {
-        List<String> fieldsNameList = fileData.read(fileName).get(HEADERS_INDEX).stream()
+        List<String> fieldsNameList = reader.read(fileName).get(HEADERS_INDEX).stream()
                                         .map(item -> item.toLowerCase())
                                         .collect(Collectors.toList());
 
@@ -64,6 +38,7 @@ public class DataDaoStream implements DataDao {
     }
 
     public List<List<String>> select(String fileName, List<String> queryFieldsList, List<String> conditionList) {
+
         List<String> fieldsNameList = getFieldsNameList(fileName);
         List<String> fieldsToSelectList = new ArrayList<>();
 
@@ -73,14 +48,14 @@ public class DataDaoStream implements DataDao {
             } else fieldsToSelectList.add(field);
         }
 
-        List<List<String>> selectedColumnContent = getSelectedColumn(fileName, fieldsToSelectList);
+        List<List<String>> selectedRowContent = getSelectedRow(fileName, conditionList);
+        List<List<String>> selectedContent = getSelectedColumn(selectedRowContent, fieldsToSelectList);
 
-        return selectedColumnContent;
+        return selectedContent;
     }
 
-    private List<List<String>> getSelectedColumn(String fileName, List<String> fieldsToSelect) {
-        List<List<String>> selectedColumnContent = fileData.read(fileName).stream()
-                .skip(1)
+    private List<List<String>> getSelectedColumn(List<List<String>> selectedRowContent, List<String> fieldsToSelect) {
+        List<List<String>> selectedColumnContent = selectedRowContent.stream()
                 .map(item -> {
                     return fieldsToSelect.stream()
                             .map(field -> item.get(fieldsNameMap.get(field))).collect(Collectors.toList());
@@ -88,12 +63,44 @@ public class DataDaoStream implements DataDao {
         return selectedColumnContent;
     }
 
+    private List<List<String>> getSelectedRow(String fileName, List<String> validConditionList) {
 
-//    private List<List<String>> getSelectedRow(String fileName, List<String> fieldsToSelect, List<String> conditionList) {
-//        List<List<String>> selectedColumnContent = getSelectedColumn(fileName, fieldsToSelect);
+        List<List<String>> selectedRowContent = reader.read(fileName).stream().skip(1)
+                .filter(predicate(validConditionList))
+                .collect(Collectors.toList());
 
-//        return selectedRowContent;
-//    }
+        return selectedRowContent;
+    }
 
+    public Predicate<List<String>> predicate (List<String> validConditionList) {
+
+        if (validConditionList.size()==0) {
+            return item -> true;
+
+        } else if (validConditionList.get(2).equals("=")) {
+            return item -> item.get(fieldsNameMap.get(validConditionList.get(0))).equals(validConditionList.get(1));
+
+        } else if (validConditionList.get(2).equals(">")) {
+            return item ->
+                    Integer.valueOf(item.get(fieldsNameMap.get(validConditionList.get(0)))) > Integer.valueOf(validConditionList.get(1));
+
+        } else if (validConditionList.get(2).equals("<")) {
+            return item ->
+                    Integer.valueOf(item.get(fieldsNameMap.get(validConditionList.get(0)))) < Integer.valueOf(validConditionList.get(1));
+
+        } else if (validConditionList.get(2).equals("<>")) {
+            return item ->
+                    Integer.valueOf(item.get(fieldsNameMap.get(validConditionList.get(0)))) != Integer.valueOf(validConditionList.get(1));
+
+        } else if (validConditionList.get(2).equals("LIKE")) {
+            return item -> {
+                Pattern patern = Pattern.compile((validConditionList.get(1)).substring(1, (validConditionList.get(1).length())-1));
+                return patern.matcher((item.get(fieldsNameMap.get(validConditionList.get(0)))).toLowerCase()).find();
+            };
+
+        }
+            return item -> false;
+
+    }
 
 }
