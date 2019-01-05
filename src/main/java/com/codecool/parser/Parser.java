581d641af4ebae7304faccf64e59c90042c43ca8
idzia <ania.idzi@gmail.com>
2019-01-05 03:00:58 +0100

    Add Where Condition Validation

--- src/main/java/com/codecool/parser/Parser.java
+++ src/main/java/com/codecool/parser/Parser.java
@@ -1,13 +1,16 @@
 package com.codecool.parser;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Parser {
 
     private List<String> parsedQueryList;
+    private List<String> validWhereConditionList;
     private static final String DELIMETER = "SELECT|FROM|WHERE|select|from|where";
     private static final String OR_AND = "OR|AND|or|and";
     private static final String REGEX = "^SELECT\\s.+\\sFROM\\s.+\\.CSV$";
@@ -25,6 +28,11 @@ public class Parser {
                     .map(item -> item.trim())
                     .skip(1)
                     .collect(Collectors.toList());
+            if (validateWhere(getWhere())) {
+                validWhereConditionList = getValidWhereCondition();
+
+            } else throw new IllegalArgumentException("WhereCondition is not valid");
+
 
         } else throw new IllegalArgumentException("Query is not valid");
 
@@ -52,9 +60,70 @@ public class Parser {
         return parsedQueryList.get(FILE_NAME);
     }
 
-    public List<String> getWhereCondition() {
-        List<String> whereConditionsList = Arrays.stream(parsedQueryList.get(WHERE_CONDITION).split(OR_AND))
-                                            .map(item-> item.trim()).collect(Collectors.toList());
-        return whereConditionsList;
+    public String getWhere() {
+        if (parsedQueryList.size()==3){
+        return parsedQueryList.get(WHERE_CONDITION).toUpperCase();
+        } else return "";
     }
+
+    public List<String> getValidWhereCondition() {
+        String whereCondition = getWhere();
+        List<String> conditionList = new ArrayList<>();
+
+        if (whereCondition.contains(" LIKE '")) {
+
+            conditionList = Arrays.stream(whereCondition.split("LIKE"))
+                    .map(item -> item.trim())
+                    .map(item -> item.toLowerCase())
+                    .collect(Collectors.toList());
+            conditionList.add("LIKE");
+
+        } else if (whereCondition.contains("<>")) {
+
+            conditionList = Arrays.stream(whereCondition.split("<>"))
+                    .map(item -> item.trim())
+                    .map(item -> item.toLowerCase())
+                    .collect(Collectors.toList());
+            conditionList.add("<>");
+
+        } else if (whereCondition.contains(">")) {
+
+            conditionList = Arrays.stream(whereCondition.split(">"))
+                    .map(item -> item.trim())
+                    .map(item -> item.toLowerCase())
+                    .collect(Collectors.toList());
+            conditionList.add(">");
+
+        } else if (whereCondition.contains("<")) {
+
+            conditionList = Arrays.stream(whereCondition.split("<"))
+                    .map(item -> item.trim())
+                    .map(item -> item.toLowerCase())
+                    .collect(Collectors.toList());
+            conditionList.add("<");
+
+        } else if (whereCondition.contains("=")) {
+
+            conditionList = Arrays.stream(whereCondition.split("="))
+                    .map(item -> item.trim())
+                    .map(item -> item.toLowerCase())
+                    .collect(Collectors.toList());
+            conditionList.add("=");
+        }
+
+        return conditionList;
+    }
+
+    public boolean validateWhere(String whereConditionString) {
+
+        Pattern compileWhereCondition = Pattern.compile(".+=.+|.+<>.+|.+>.+|.+<.+|.+[^<>=]\\sLIKE\\s'.+'");
+//".+=.+|.+<>.+|.+>[^<].+|.+[^>]<.+|.+[^<>=]\\sLIKE\\s'.+'");
+        if (compileWhereCondition.matcher(whereConditionString).matches()||whereConditionString.equals("")) {
+            return true;
+        }
+        return false;
+    }
+
+
+
 }
